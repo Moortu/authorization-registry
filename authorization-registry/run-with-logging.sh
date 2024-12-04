64274f4a437fe64842ec898d7d0c2019ef7930c0
joeri1324 <jsleegers@hotmail.com>
2024-12-04 10:05:02 +0000

    bump ishare version

--- authorization-registry/run-with-logging.sh
+++ authorization-registry/run-with-logging.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-RUST_LOG=tower_http=debug,authorization_registry=debug cargo run -p authorization_registry -- "$@"
+RUST_LOG=tower_http=debug,authorization_registry=debug,ishare=debug cargo run -p authorization_registry -- "$@"
