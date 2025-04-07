fe89ff26ac39143da5b4e5ab652dcb5f728e8240
joeri1324 <jsleegers@hotmail.com>
2025-04-07 13:33:30 +0100

    different version

--- authorization-registry/src/routes/capabilities.rs
+++ authorization-registry/src/routes/capabilities.rs
@@ -5,14 +5,14 @@ use axum::{
     routing::get,
     Json, Router,
 };
+use reqwest::StatusCode;
 use serde::Serialize;
 
 use crate::{
-    error::AppError,
+    error::{AppError, ExpectedError},
     services::ishare_provider::{
         Capabilities, CapabilitiesInfo, SupportedFeature, SupportedFeatures, SupportedVersion,
     },
-    utils::extract_bearer_token,
     AppState,
 };
 use utoipa::ToSchema;
@@ -55,7 +55,7 @@ pub fn create_capabilities(party_id: &str, api_url: &str, show_private: bool) ->
             party_id: party_id.to_owned(),
             ishare_roles: vec!["AuthorizationRegistry".to_owned()],
             supported_versions: vec![SupportedVersion {
-                version: "0.1.0".to_owned(),
+                version: "1.7".to_owned(),
                 supported_features,
             }],
         },
@@ -65,6 +65,31 @@ pub fn create_capabilities(party_id: &str, api_url: &str, show_private: bool) ->
 #[derive(Serialize, ToSchema)]
 struct CapabilitiesResponse {
     capabilities_token: String,
+}
+
+pub fn extract_bearer_token(header_map: &HeaderMap) -> Result<Option<String>, AppError> {
+    let auth_header = match header_map.get("Authorization") {
+        Some(header) => header,
+        None => return Ok(None),
+    };
+
+    match auth_header
+        .to_str()
+        .context("Removing bearer prefix from auth header")?
+        .strip_prefix("Bearer ")
+    {
+        Some(token) => {
+            return Ok(Some(token.to_owned()));
+        }
+        None => {
+            return Err(AppError::Expected(ExpectedError {
+                status_code: StatusCode::BAD_REQUEST,
+                message: "Missing bearer prefix in Authorization header".to_owned(),
+                reason: "Missing bearer prefix in Authorization header".to_owned(),
+                metadata: None,
+            }));
+        }
+    };
 }
 
 /// Retrieve iSHARE capabilities
@@ -88,12 +113,13 @@ async fn get_capabilities(
     State(app_state): State<AppState>,
 ) -> Result<Json<CapabilitiesResponse>, AppError> {
     let show_private = match extract_bearer_token(&header_map) {
-        Err(_) => false,
-        Ok(raw_token) => {
+        Err(e) => return Err(e),
+        Ok(Some(raw_token)) => {
             let _token = app_state.server_token.decode_token(&raw_token)?;
 
             true
         }
+        Ok(None) => false,
     };
 
     let scheme = match header_map.get("X-Forwarded-Proto") {
@@ -129,6 +155,31 @@ mod test {
     use tower::ServiceExt;
 
     use super::super::super::test_helpers::helpers::*;
+
+    #[sqlx::test]
+    async fn auth_header_not_bearer_plus_value(
+        _pool_options: PgPoolOptions,
+        conn_option: PgConnectOptions,
+    ) -> sqlx::Result<()> {
+        let db = init_test_db(&conn_option).await;
+        let app = get_test_app(db);
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/capabilities")
+                    .header("Host", "Example.com")
+                    .header("Authorization", "Chicken token")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+
+        Ok(())
+    }
 
     #[sqlx::test]
     async fn test_get_public_capabilities(
