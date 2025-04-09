56451747d3bc0372613f6cea60d6d9ee7d1a7676
joeri1324 <jsleegers@hotmail.com>
2025-04-09 16:07:46 +0100

    disable certificate validation

--- authorization-registry/src/services/ishare_provider.rs
+++ authorization-registry/src/services/ishare_provider.rs
@@ -348,21 +348,21 @@ impl SatelliteProvider for ISHAREProvider {
             .await
             .context(format!("error validating ishare party '{}'", &client_id))?;
 
-        if !self
-            .ishare
-            .validate_party_certificate(&client_assertion_token, &party_info)
-            .context(format!(
-                "Error validating party certificate for ishare party: '{}'",
-                &client_id
-            ))?
-        {
-            return Err(AppError::Expected(ExpectedError {
-                status_code: StatusCode::UNAUTHORIZED,
-                message: "x5c header does not match any of the certificates from parties endpoint at the iSHARE satelite".to_owned(),
-                  reason: "The client assertion x5c does not match any of the valid tokens from /parties".to_owned(),
-                  metadata: None
-            }));
-        }
+        // if !self
+        //     .ishare
+        //     .validate_party_certificate(&client_assertion_token, &party_info)
+        //     .context(format!(
+        //         "Error validating party certificate for ishare party: '{}'",
+        //         &client_id
+        //     ))?
+        // {
+        //     return Err(AppError::Expected(ExpectedError {
+        //         status_code: StatusCode::UNAUTHORIZED,
+        //         message: "x5c header does not match any of the certificates from parties endpoint at the iSHARE satelite".to_owned(),
+        //           reason: "The client assertion x5c does not match any of the valid tokens from /parties".to_owned(),
+        //           metadata: None
+        //     }));
+        // }
 
         let company_id =
             company_store::insert_if_not_exists(&client_id, &party_info.party_name, &self.db)
