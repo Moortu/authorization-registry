c6d055775832c4e87e9839539cbc9fb8195f56d8
siarhei.m <siarhei.m@cgt.group>
2025-11-17 09:05:15 +0100

    feat(auth): Switch auth check back on for opensource AR

--- authorization-registry/src/routes/admin.rs
+++ authorization-registry/src/routes/admin.rs
@@ -57,13 +57,13 @@ pub fn get_admin_routes(
                 .put(replace_policy_in_policy_set)
                 .get(get_policy),
         )
-        .layer(Extension(app_state.clone()))
-        .layer(from_fn_with_state(server_token.clone(), extract_role_middleware))
-        .layer(from_fn(extract_human_middleware))
         .layer(from_fn_with_state(
             vec!["dexspace_admin".to_owned()],
             auth_role_middleware,
         ))
+        .layer(from_fn(extract_human_middleware))
+        .layer(from_fn_with_state(server_token.clone(), extract_role_middleware))
+        .layer(Extension(app_state.clone()))
 }
 
 
